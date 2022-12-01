cded125e039a73e049e5dee8afc7e26a9cff0ec0
Jon Kruger <84333+JonKruger@users.noreply.github.com>
2022-12-01 07:24:05 -0500

    part 2

--- day1/day1.rb
+++ day1/day1.rb
@@ -2,6 +2,10 @@ def part1(input)
   input.split("\n").join(",").split(",,").map { |a| a.split(",") }.map { |a| a.map(&:to_i) }.map(&:sum).max
 end
 
+def part2(input)
+  elves = input.split("\n").join(",").split(",,").map { |a| a.split(",") }.map { |a| a.map(&:to_i) }.map(&:sum).sort.reverse[0..2].sum
+end
+
 test_input = <<-INPUT
 1000
 2000
@@ -21,6 +25,9 @@ INPUT
 result = part1(test_input)
 raise result.inspect if result != 24000
 
+result = part2(test_input)
+raise result.inspect if result != 45000
 
 input = File.read("input.txt")
-puts part1(input)
+puts "part1 - #{part1(input)}"
+puts "part2 - #{part2(input)}"
