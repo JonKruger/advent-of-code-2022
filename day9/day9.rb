b261a33a905966562569107ea189bb526696734e
Jon Kruger <84333+JonKruger@users.noreply.github.com>
2022-12-09 22:06:14 -0500

    refactored part 1 to get ready for part 2

--- day9/day9.rb
+++ day9/day9.rb
@@ -9,6 +9,31 @@ class Location
   end
 end
 
+class Section
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @visited_locations = [[x,y]]
+  end
+
+  def move(x_transform, y_transform)
+    @x += x_transform
+    @y += y_transform
+    @visited_locations << [x,y]
+    nil
+  end
+
+  def location
+    [x,y]
+  end
+
+  def visited_locations
+    @visited_locations.uniq.freeze
+  end
+end
+
 HEAD_TRANSFORMS = {
   "U" => [0,1],
   "D" => [0,-1],
@@ -72,36 +97,56 @@ TRANSFORMS = {
   [-1,-1,0,-1] => [0,0], # down
 }
 
-def process_move(current_head_location, current_tail_location, locations, actions)
+def process_move(sections, actions)
   action = actions.shift
   head_transform = HEAD_TRANSFORMS[action]
-  new_head_location = [current_head_location[0] + head_transform[0], current_head_location[1] + head_transform[1]]
 
-  transform_key = [current_tail_location[0] - current_head_location[0], current_tail_location[1] - current_head_location[1], head_transform[0], head_transform[1]]
-  tail_transform = TRANSFORMS[transform_key]
-  raise "missing tail transform for #{transform_key}" unless tail_transform
+  # puts("move #{head_transform.inspect} from #{sections.map(&:location).inspect}...")
 
-  new_tail_location = [current_tail_location[0] + tail_transform[0], current_tail_location[1] + tail_transform[1]]
-  # puts action, tail_transform, new_tail_location
+  sections.each_with_index do |section, index|
+    head = section
+    tail = sections[index + 1]
 
-  locations[new_tail_location] ||= Location.new(*new_tail_location)
-  locations[new_tail_location].visited = true
-  puts("#{action} -> #{new_head_location.inspect}, #{new_tail_location.inspect}")
-  [new_head_location, new_tail_location, locations, actions]
+    next unless tail
+
+    previous_head_location = head.location
+    head.move(*head_transform)
+
+    transform_key = [tail.x - previous_head_location[0], tail.y - previous_head_location[1], head_transform[0], head_transform[1]]
+    tail_transform = TRANSFORMS[transform_key]
+    raise "missing tail transform for #{transform_key}" unless tail_transform
+
+    tail.move(*tail_transform)
+    head_transform = tail_transform
+  end
+
+  # puts("... to #{sections.map(&:location).inspect}")
+
+  [sections, actions]
 end
 
-def part1(input)
-  locations = { [0,0] => Location.new(0,0) }
+def part1(input, rope_size)
+  sections = (0...rope_size).map { |_| Section.new(0,0) }
   actions = input.split("\n").map { |line| line.split(" ") }.map { |direction, amount| [direction] * amount.to_i }.flatten
-  current_head_location = [0,0]
-  current_tail_location = [0,0]
 
   while actions.any?
-    current_head_location, current_tail_location, locations, actions = process_move(current_head_location, current_tail_location, locations, actions)
+    sections, actions = process_move(sections, actions)
   end
-  puts "visited locations - #{locations.select { |coords, location| location.visited }.keys.sort.inspect}"
-  locations.select { |coords, location| location.visited }.size
+  puts "visited locations - #{sections.last.visited_locations.sort.inspect}"
+  sections.last.visited_locations.size
 end
+
+sections = [Section.new(0,0), Section.new(0,0)]
+actions = ["R"]
+sections, actions = process_move(sections, actions)
+raise unless sections[0].location == [1,0]
+raise unless sections[1].location == [0,0]
+
+sections = [Section.new(1,0), Section.new(0,0)]
+actions = ["R"]
+sections, actions = process_move(sections, actions)
+raise unless sections[0].location == [2,0]
+raise unless sections[1].location == [1,0]
 
 test_input = <<-INPUT
 R 4
@@ -113,12 +158,12 @@ D 1
 L 5
 R 2
 INPUT
-result = part1(test_input)
+result = part1(test_input, 2)
 raise result.inspect unless result == 13
 
 # result = part2(test_input)
 # raise result.inspect unless result == 8
 
 input = File.read("input.txt")
-puts "part1 = #{part1(input)}"
+puts "part1 = #{part1(input, 2)}"
 # puts "part2 = #{part2(input)}"
