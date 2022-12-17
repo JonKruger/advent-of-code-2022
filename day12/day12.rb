c8d54965d82f23120294bd214fe96953c4953ebe
Jon Kruger <84333+JonKruger@users.noreply.github.com>
2022-12-16 22:18:35 -0500

    calculating distance to end

--- day12/day12.rb
+++ day12/day12.rb
@@ -4,6 +4,7 @@ class Square
   END_VALUE = 1000000
 
   attr_reader :char, :value, :neighbors
+  attr_accessor :distance_to_end, :location
 
   def initialize(char)
     @char = char
@@ -32,6 +33,11 @@ class Square
   def end?
     char == "E"
   end
+
+  def distance_to_end=(value)
+    raise unless value.is_a?(Integer)
+    @distance_to_end = value
+  end
 end
 
 
@@ -48,6 +54,39 @@ class Grid
 
   def end_square
     squares.select(&:end?).first
+  end
+
+  def calculate_distance_to_end
+    end_square.distance_to_end = 0
+    processed_squares = [end_square]
+    while processed_squares.size < squares.size
+      processed_squares = processed_squares.map do |square|
+        square.neighbors.each do |n|
+          n.distance_to_end = n.distance_to_end.nil? ? square.distance_to_end + 1 : [n.distance_to_end, square.distance_to_end + 1].min
+        end
+        [square] + square.neighbors
+      end.flatten.uniq
+    end
+    print_grid(:distance_to_end)
+  end
+
+  def print_grid(mode)
+    row_count = squares.map { |s| s.location[0] }.max
+    column_count = squares.map { |s| s.location[1] }.max
+
+    output = (0..row_count).map do |row_index|
+      (0..column_count).map do |column_index|
+        square = squares.select { |s| s.location == [row_index, column_index] }.first
+        case mode
+        when :distance_to_end
+          (square.distance_to_end&.to_s || " ").rjust(4, " ")
+          # "-"
+        else
+          "?"
+        end
+      end.join + "\n"
+    end.join
+    puts output
   end
 end
 
@@ -85,6 +124,7 @@ def build_grid(input)
   (0...num_rows).each do |row|
     (0...num_cols).each do |col|
       square = rows[row][col]
+      square.location = [row, col]
       square.neighbors << rows[row][col-1] if col > 0
       square.neighbors << rows[row][col+1] if col < num_cols - 1
       square.neighbors << rows[row-1][col] if row > 0
@@ -101,6 +141,7 @@ end
 
 def part1(input)
   grid = build_grid(input)
+  grid.calculate_distance_to_end
   sequences = [Sequence.new([grid.start_square])]
   steps = 0
   while !sequences.any?(&:complete?)
@@ -122,3 +163,6 @@ INPUT
 
 result = part1(test_input)
 raise result.inspect unless result == 31
+
+# input = File.read("input.txt")
+# puts("part1 = #{part1(input)}")
