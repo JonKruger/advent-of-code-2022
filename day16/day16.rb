8cc27d17015297acf9cd312f53bdb1ae2efa2d1d
Jon Kruger <84333+JonKruger@users.noreply.github.com>
2022-12-22 08:48:34 -0500

    works on simple example

--- day16/day16.rb
+++ day16/day16.rb
@@ -56,10 +56,10 @@ end
 
 class Path
   attr_reader :time_elapsed, :current_location, :pressure_released, :time_limit, :valve_path, :value_paths,
-              :open_valves, :flow_rate, :open_valves
+              :open_valves, :flow_rate, :open_valves, :log
 
   def initialize(time_elapsed, current_location, pressure_released, time_limit, valve_path,
-                 value_paths, open_valves, valve_hash, flow_rate)
+                 value_paths, open_valves, valve_hash, flow_rate, log)
     raise if value_paths.nil?
     @time_elapsed = time_elapsed
     @current_location = current_location
@@ -70,11 +70,12 @@ class Path
     @open_valves = open_valves.dup
     @valve_hash = valve_hash
     @flow_rate = flow_rate
+    @log = log.dup
   end
 
   def clone
     Path.new(time_elapsed, current_location, pressure_released, time_limit, valve_path, value_paths, open_valves,
-             @valve_hash, flow_rate)
+             @valve_hash, flow_rate, log)
   end
 
   def valves
@@ -83,6 +84,10 @@ class Path
 
   def time_left
     time_limit - time_elapsed
+  end
+
+  def complete?
+    time_left == 0
   end
 
   def current_valve
@@ -95,20 +100,50 @@ class Path
 
   def move_to(new_location)
     raise "no tunnel from #{current_location} to #{new_location}" unless current_valve.can_move_to?(new_location)
-    @current_location = new_location
     tick
+    @current_location = new_location
+    @log << "Move to #{new_location}"
+    valve_path << new_location
   end
 
   def tick
+    raise "out of time" if time_elapsed == time_limit
     @time_elapsed += 1
     @pressure_released += flow_rate
+    @log << "[#{time_elapsed}] Values #{open_valves.join(", ")} are open, releasing #{flow_rate} pressure (total #{pressure_released})"
   end
 
   def open_valve
     tick
     @open_valves << current_location
+    @log << "Open #{current_location}"
     @flow_rate += current_valve.flow_rate
     @value_paths = @value_paths.select { |path| path.destination != current_location }
+  end
+
+  def value_paths_from(start_location)
+    @value_paths.select { |value_path| value_path.start_location == start_location }
+  end
+
+  def travel_value_path(value_path)
+    raise unless current_location == value_path.start_location
+    value_path.path[1..].each { |location| move_to(location) }
+    open_valve
+  end
+
+  def travel_all_possible_value_paths
+    value_paths_from_here = value_paths_from(current_location)
+    if value_paths_from_here.empty?
+      # nowhere to go, just let time elapse
+      time_left.times { tick }
+      [self]
+    else
+      value_paths_from_here.map do |value_path|
+        cloned_path = clone
+        cloned_path.travel_value_path(value_path)
+        cloned_path
+      end
+    end
   end
 end
 
@@ -133,7 +168,7 @@ class Map
     map_value_paths
     @paths = [
       Path.new(0, "AA", 0, time_limit, ["AA"],
-               @value_paths, [], @valve_hash, 0)
+               @value_paths, [], @valve_hash, 0, [])
     ]
   end
 
@@ -143,11 +178,6 @@ class Map
 
   def valves
     @valve_hash.values
-  end
-
-  def travel_value_path(value_path)
-    raise unless current_location == value_path.start_location
-    value_path.path[1..].each { |location| move_to(location) }
   end
 
   def map_value_paths
@@ -163,26 +193,37 @@ class Map
   end
 
   def identify_next_stops(path)
+    paths = []
+
     current_valve = @valve_hash[path.last]
     if path.size > 1 && current_valve.flow_rate > 0
       # puts("done")
-      return [path]
+      paths << path
     end
 
-    current_valve.leads_to
+    paths += current_valve.leads_to
                  .select { |next_valve| !path.include?(next_valve) }
                  .reduce([]) do |new_paths, next_valve|
       # puts("next #{next_valve}")
       new_paths += identify_next_stops(path + [next_valve])
     end
-  end
 
-  def value_paths_from(start_location)
-    @value_paths.select { |path| path.start_location == start_location }
+    # if there are two paths to the same place, keep the shortest one
+    paths = paths.group_by(&:last).map { |last, paths_to_last| paths_to_last.sort_by(&:size).first }
+
+    paths
   end
 
   def maximize_pressure_released
+    while paths.any? { |path| !path.complete? }
+      puts "value_path_step with #{paths.size} paths (#{paths.select(&:complete?).size} complete)"
+      sleep 0.1
+      value_path_step
+    end
+  end
 
+  def value_path_step
+    @paths = paths.map(&:travel_all_possible_value_paths).flatten
   end
 end
 
@@ -209,8 +250,9 @@ raise unless map.paths[0].flow_rate == 0
 raise unless map.paths[0].time_elapsed == 0
 raise unless map.paths[0].current_location == "AA"
 
-raise unless map.value_paths_from("AA").size == 3
-aa_jj_path = map.value_paths_from("AA").select { |path| path.destination == "JJ" }.first
+# one path to each open valve
+raise map.paths[0].value_paths_from("AA").size.inspect unless map.paths[0].value_paths_from("AA").size == 6
+aa_jj_path = map.paths[0].value_paths_from("AA").select { |path| path.destination == "JJ" }.first
 raise unless aa_jj_path.distance == 2
 raise unless aa_jj_path.flow_rate == 21
 raise unless aa_jj_path.flow_per_minute == 10.5
@@ -241,3 +283,31 @@ raise unless map.paths[0].time_elapsed == 3
 raise unless map.paths[0].current_location == "CC"
 
 map = Map.parse(test_input, 30)
+map.value_path_step
+raise unless map.paths.size == 6
+raise map.paths.map(&:valve_path).sort.inspect unless map.paths.map(&:valve_path).sort ==  [["AA", "BB"], ["AA", "DD"], ["AA", "DD", "CC"], ["AA", "DD", "EE"], ["AA", "DD", "EE", "FF", "GG", "HH"], ["AA", "II", "JJ"]]
+raise map.paths.map(&:open_valves).sort.inspect unless map.paths.map(&:open_valves).sort == [["BB"], ["CC"], ["DD"], ["EE"], ["HH"], ["JJ"]]
+
+map.value_path_step
+raise map.paths.size.inspect unless map.paths.size == 30
+path = map.paths.select { |path| path.valve_path == ["AA", "DD", "CC", "BB"] }[0]
+raise path.open_valves.inspect unless path.open_valves == ["DD", "BB"]
+raise path.time_elapsed.inspect unless path.time_elapsed == 5
+raise path.pressure_released.inspect unless path.pressure_released == 60
+
+map.value_path_step
+map.value_path_step
+map.value_path_step
+map.value_path_step
+map.value_path_step
+# map.paths.select { |p| p.valve_path[0..4] == ["AA", "DD", "CC", "BB", "AA"]}[0].log.each { |l| puts l }
+# puts map.paths.map(&:pressure_released).sort.inspect
+
+map = Map.parse(test_input, 30)
+map.maximize_pressure_released
+# puts map.paths.map(&:pressure_released).sort.tally.inspect
+puts map.paths.map(&:pressure_released).max
+
+# TODO
+# map_value_paths needs to go from each valve to every other valve - you might want to
+# pass a closed valve to get to a more valuable one first
